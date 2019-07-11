c383f1589f071c8083f42440cc35527633621884
RJ Salamanca <salamancaronito@gmail.com>
2019-07-11 14:46:39 -0400

    updated models updated routers

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    cors = require('cors');
 
 const indexRouter = require('./routes/index'),
     usersRouter = require('./routes/users'),
     version1Router = require('./routes/version1');
+
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+};
 
 const app = express();
 
@@ -14,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
