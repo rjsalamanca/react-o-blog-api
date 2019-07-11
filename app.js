5142eaf157754c804cf4cd4e13e167d8bb95ed8f
RJ Salamanca <salamancaronito@gmail.com>
2019-07-11 10:01:59 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan');
+
+const indexRouter = require('./routes/index'),
+    usersRouter = require('./routes/users'),
+    version1Router = require('./routes/version1');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/v1', version1Router);
+module.exports = app;
